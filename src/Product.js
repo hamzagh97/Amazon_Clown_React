f2e131403436246973702f1794076eeb2c61a9ef
hamza ghazouani <hamzagh168@gmail.com>
2022-07-22 22:30:09 +0100

    add files

---
+++ src/Product.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Product.css';
+
+function Product(props) {
+  return (
+    <div className='product'>
+        <div className='title'>
+        { props.title }
+        </div>
+        <img alt='product_img' src={ props.image } className='product_img'/>
+        <div className='product_footer'>more</div>
+    </div>
+  )
+}
+
+export default Product
